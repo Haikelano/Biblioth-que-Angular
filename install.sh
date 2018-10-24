d75f7f5559dbe6663de60c7f7fe7812124766405
Haykel Maaoui <haykel.maaoui@gmail.com>
2018-10-24 16:44:55 +0200

    first commit

---
+++ install.sh
@@ -0,0 +1,14 @@
+ng g c auth/signup
+ng g c auth/signin
+ng g c book-list
+ng g c book-list/single-book
+ng g c book-list/book-form
+ng g c header
+ng g s services/auth
+ng g s services/books
+ng g s services/auth-guard
+npm install bootstrap
+npm install --save bootstrap@3.3.7
+npm audit fix
+
+echo " Bien tous va bien"
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  constructor() {
+    var config = {
+      apiKey: "AIzaSyDq3wxID35hh4tP0RPdxEGpgJYW9-zclMA",
+      authDomain: "bookangular-af3dd.firebaseapp.com",
+      databaseURL: "https://bookangular-af3dd.firebaseio.com",
+      projectId: "bookangular-af3dd",
+      storageBucket: "gs://bookangular-af3dd.appspot.com",
+      messagingSenderId: "1056898956453"
+    };
+    firebase.initializeApp(config);
+  }
 }
